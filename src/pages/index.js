7df6d898340a3b5a76a3283e46ce55de457cd2b2
Annie Taylor CHEN <annietaylorchen@gmail.com>
2020-03-02 21:44:14 +0100

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,21 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Helmet } from "react-helmet"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+
 import SEO from "../components/seo"
 
 const IndexPage = () => (
+  <>
+  <Helmet>
+    <script defer src="https://cse.google.com/cse.js?cx=003624295726000675291:iawj2kriyxg"></script>
+  </Helmet>
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Here goes the Google search:</h1>
+    <div className="gcse-search"></div>
   </Layout>
+  </>
 )
 
 export default IndexPage
